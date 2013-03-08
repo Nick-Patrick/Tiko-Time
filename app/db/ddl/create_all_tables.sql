8c13caaa93dfc58eda17495126871caee5e15d77
Nick Patrick <n.patrick@outlook.com>
2013-03-08 12:14:55 +0000

    create events custom added

--- app/db/ddl/create_all_tables.sql
+++ app/db/ddl/create_all_tables.sql
@@ -122,3 +122,11 @@ create table IF NOT EXISTS users
 	phone varchar(30),
 	disabled varchar(1)	
 );
+
+CREATE TABLE IF NOT EXISTS events_custom
+(
+	events_custom_id int(5),
+	event_template_id int(5),
+	events_name varchar(50),
+	event_name_value varchar(250)
+);
---
+++ app/db/ddl/create_events_custom.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS events_custom
+(
+	events_custom_id int(5),
+	event_template_id int(5),
+	events_name varchar(50),
+	event_name_value varchar(250)
+);
